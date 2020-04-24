64d51f06761ee5fe314f316e75a9b16adb8570b2
zrebec <zrebec@zrebec.sk>
2020-04-24 16:51:16 +0200

    - Layout changes - Javascript key press added functionality - Fake logic fo save

--- page.js
+++ page.js
@@ -4,6 +4,45 @@ var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ','];
 var special = ['+', 'undo', 'submit', 'clear'];
 var allowedInputs = numbers.concat(special);
 
+function toggleFullScreen() {
+    console.log();
+    
+    if(document.fullscreen === false) {
+        document.body.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+        return;
+    }
+    
+}
+
+window.addEventListener("keypress", function(e) {
+    var x = e.which || e.keyCode;
+    var key = String.fromCharCode(x);
+
+    if(numbers.includes(key)) {
+        addNumber(key);
+    } else if (x === 46 || x === 44) {
+        // Pressed dash on numpad or pressed dot
+        addNumber(',');
+    } else if (x === 43 || x === 45 ) {
+        // Pressed + (PLUS) or - (MINUS)
+        addNumber('+');
+    } else if (x === 117 || x === 85) {
+        addNumber('undo');
+        // Pressed "u" or "U" as UNDO
+    } else if (x === 99 || x === 67) {
+        // Pressed "c" or "C" as CLEAR
+        addNumber('clear');
+    } else if (x === 13) {
+        // Pressed ENTER
+        addNumber('submit');
+    } else {
+        console.log('Incorrect key with code ' + x + " where string is " + key);
+    }
+
+});
+
 function addNumber(x) {
 
     // If input value isn't in allowed array, return calc
@@ -20,6 +59,16 @@ function addNumber(x) {
             break;
             case 'submit':
                 // Submit button save changes to database
+
+                // Just fake logic yet
+                total = '';
+                display.innerHTML = 'SAVING...';
+
+                // Incoming set to true after save to database because we want
+                // already outgoing then
+                incoming = true;
+                addNumber('+');
+
                 // Logic for submit
                 break;
             case 'clear':
@@ -32,14 +81,24 @@ function addNumber(x) {
                 if (incoming === true) {
                     document.getElementById('button-plus').className = 
                         document.getElementById('button-plus').className.replace(' button-plus', '');
-                    incoming = true;
+                    document.getElementById('display').className = 
+                        document.getElementById('display').className.replace('incoming', 'outgoing');
+                    incoming = false;
                 } else {
                     document.getElementById('button-plus').className += ' button-plus';
+                    document.getElementById('display').className = 
+                        document.getElementById('display').className.replace('outgoing', 'incoming');
                     incoming = true;
                 }
+                
                 break;
         default:
-            //if total()
+            // Check if decimal place already existing
+            if (x === ',' && total.includes(',')) {
+                // Decimal place already exists
+                return;
+            }
+
             total = total + x;
             display.innerHTML = total;
     }
